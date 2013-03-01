7b36f902829725c7b1f745c2e3c9aa2f9a244632
Ryan Artecona <ryanartecona@gmail.com>
2013-02-28 23:59:16 -0600

    added custom config for Slate app

---
+++ slate.js
@@ -0,0 +1,128 @@
+// Ryan Artecona
+// config for Slate app (https://github.com/jigish/slate)
+
+slate.configAll({
+	'nudgePercentOf': 'screenSize',
+	'resizePercentOf': 'screenSize',
+	'repeatOnHoldOps': 'move,resize,nudge',
+	'secondsBeforeRepeat': 0.3,
+	'secondsBetweenRepeat': 0.08
+});
+
+
+var numCols = 12;
+var numRows = 8;
+
+var menuBarHeight = 20;
+
+
+// Reusable ops
+
+var pushLeft = slate.operation('push', {'direction': 'left'});
+var pushRight = slate.operation('push', {'direction': 'right'});
+var pushUp = slate.operation('push', {'direction': 'up'});
+var pushDown = slate.operation('push', {'direction': 'down'});
+
+var nudgeLeft = function(window) {
+	var winX = window.rect().x;
+	var screenWidth = window.screen().visibleRect().width;
+	var colWidth = screenWidth / numCols;
+
+	var nudgeByX = Math.floor(Math.max(0, winX % colWidth)) || Math.ceil(colWidth);
+
+	window.doop(S.op('nudge', {
+		'x': '-'+nudgeByX,
+		'y': '+0',
+	}));
+};
+
+var nudgeRight = function(window) {
+	var winX = window.rect().x;
+	var screenWidth = window.screen().visibleRect().width;
+	var colWidth = screenWidth / numCols;
+
+	var nudgeByX = Math.floor(Math.max(0, colWidth-(winX % colWidth))) || Math.ceil(colWidth);
+
+	window.doop(S.op('nudge', {
+		'x': '+'+nudgeByX,
+		'y': '+0'
+	}));
+};
+
+var nudgeUp = function(window) {
+	var winY = window.rect().y - menuBarHeight;
+	var screenHeight = window.screen().visibleRect().height;
+	var rowHeight = screenHeight / numRows;
+
+	var nudgeByY = Math.floor(Math.max(0, winY % rowHeight)) || Math.ceil(rowHeight);
+
+	window.doop(S.op('nudge', {
+		'x': '+0',
+		'y': '-'+nudgeByY
+	}));
+};
+
+var nudgeDown = function(window) {
+	var winY = window.rect().y - menuBarHeight;
+	var screenHeight = window.screen().visibleRect().height;
+	var rowHeight = screenHeight / numRows;
+
+	var nudgeByY = Math.floor(Math.max(0, rowHeight-(winY % rowHeight))) || Math.ceil(rowHeight);
+
+	window.doop(S.op('nudge', {
+		'x': '+0',
+		'y': '+'+nudgeByY
+	}));
+};
+
+
+var resizeBy = function(amtByX, amtByY) {
+	return function(window) {
+		var winWidth = window.rect().width;
+		var winHeight = window.rect().height;
+		var screenWidth = window.screen().visibleRect().width;
+		var screenHeight = window.screen().visibleRect().height;
+		var colWidth = screenWidth / numCols;
+		var rowHeight = screenHeight / numRows;
+
+		var widthOffset = (colWidth * (amtByX>0)) + ((winWidth % colWidth) * -amtByX);
+		widthOffset = amtByX && (Math.floor(Math.max(0, widthOffset)) || Math.ceil(colWidth));
+
+		var heightOffset = (rowHeight * (amtByY>0)) + ((winHeight % rowHeight) * -amtByY);
+		heightOffset = amtByY && (Math.floor(Math.max(0, heightOffset)) || Math.ceil(rowHeight));
+
+		window.resize({
+			'width': winWidth + (amtByX * widthOffset),
+			'height': winHeight + (amtByY * heightOffset)
+		});
+	}
+};
+
+var extendWidth = resizeBy(1, 0);
+var contractWidth = resizeBy(-1, 0);
+var extendHeight = resizeBy(0, 1);
+var contractHeight = resizeBy(0, -1);
+
+
+// Key Bindings
+slate.bindAll({
+	// 'key:mod1,mod2': [operation, isRepeatable]
+
+	// growing/shrinking current window
+	'left:ctrl': [contractWidth, true],
+	'right:ctrl': [extendWidth, true],
+	'up:ctrl': [contractHeight, true],
+	'down:ctrl': [extendHeight, true],
+
+	// moving current window
+	'left:ctrl,alt': [nudgeLeft, true], 
+	'right:ctrl,alt': [nudgeRight, true],
+	'up:ctrl,alt': [nudgeUp, true],
+	'down:ctrl,alt': [nudgeDown, true],
+
+	// shooting current window to sides/corners
+	'left:shift,ctrl': [pushLeft, true],
+	'right:shift,ctrl': [pushRight, true],
+	'up:shift,ctrl': [pushUp, true],
+	'down:shift,ctrl': [pushDown, true]
+});
