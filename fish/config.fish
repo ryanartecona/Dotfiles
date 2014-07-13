0286261da68aae1ee0e9c384b3a40e8481ea5dfe
Ryan Artecona <ryanartecona@gmail.com>
2014-07-12 21:21:57 -0400

    add $PATH and fish_color vars

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,56 @@
 if which hub  >/dev/null
   alias g=hub
 end
+
+if status --is-interactive
+  set -g EDITOR                           (which vim)
+
+  set -g fish_prompt_git_prefix           ' | '
+
+  set -g fish_color_autosuggestion        black
+  set -g fish_color_command               magenta --bold
+  set -g fish_color_comment               red
+  set -g fish_color_cwd                   green
+  set -g fish_color_cwd_root              red
+  set -g fish_color_error                 red --bold
+  set -g fish_color_escape                cyan
+  set -g fish_color_git_ahead             yellow --bold
+  set -g fish_color_git_clean             green
+  set -g fish_color_git_dirty             red
+  set -g fish_color_history_current       cyan
+  set -g fish_color_host                  black --bold
+  set -g fish_color_match                 cyan
+  set -g fish_color_normal                normal
+  set -g fish_color_operator              cyan
+  set -g fish_color_param                 magenta
+  set -g fish_color_prompt_delimiter      black
+  set -g fish_color_prompt_symbol         blue --bold
+  set -g fish_color_quote                 cyan --bold
+  set -g fish_color_redirection           normal
+  set -g fish_color_search_match          --background=purple
+  set -g fish_color_status                red
+  set -g fish_color_user                  black --bold
+  set -g fish_color_valid_path            cyan
+end
+
+
+function allowed_paths --description "User-allowed \$path dirs"
+  echo $HOME/bin
+  echo /usr/local/share/npm/bin
+  echo $HOME/.rvm/bin
+  echo $HOME/.cabal/bin
+  echo $HOME/Library/Haskell/bin
+  echo /export/apps/xtools/bin
+  echo /Applications/ghc-7.8.2.app/Contents/bin
+  echo /usr/local/share/python
+  echo /usr/local/bin
+  echo /usr/local/sbin
+  echo /usr/bin
+  echo /bin
+  echo /usr/sbin
+  echo /sbin
+end
+
+if status --is-login
+  set -g PATH (allowed_paths)
+end
