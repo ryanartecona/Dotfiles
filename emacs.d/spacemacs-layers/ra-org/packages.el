9b2625c121239ae3364ebaa2cb89e7dada097fa7
Ryan Artecona <ryanartecona@gmail.com>
2015-06-03 10:47:59 -0400

    disable my bespoke org config, since the official one improved

--- emacs.d/spacemacs-layers/ra-org/packages.el
+++ emacs.d/spacemacs-layers/ra-org/packages.el
@@ -6,25 +6,15 @@
 
 (setq ra-org-excluded-packages '())
 
-(defun ra-org/init-org ()
-  "Define decent org-mode evil keybindings."
-  (use-package org
-    :config
-    (evil-leader/set-key-for-mode 'org-mode
-      "mh" 'org-metaleft
-      "mj" 'org-metadown
-      "mk" 'org-metaup
-      "ml" 'org-metaright
-      "mH" 'org-shiftmetaleft
-      "mJ" 'org-shiftmetadown
-      "mK" 'org-shiftmetaup
-      "mL" 'org-shiftmetaright
-      "mS" 'org-sort
-      "mr" 'org-refile
-      "mpj" 'org-priority-down
-      "mpk" 'org-priority-up
-      "mt" nil "mtl" 'org-toggle-link-display
-      "mi" 'org-clock-in
-      "mo" 'org-clock-out
-     ))
-  )
+;; (defun ra-org/init-org ()
+;;   "Define decent org-mode evil keybindings."
+;;   (use-package org
+;;     :config
+;;     (evil-leader/set-key-for-mode 'org-mode
+;;       "mS" 'org-sort
+;;       "mr" 'org-refile
+;;       "mpj" 'org-priority-down
+;;       "mpk" 'org-priority-up
+;;       "mt" nil "mtl" 'org-toggle-link-display
+;;      ))
+;;   )
