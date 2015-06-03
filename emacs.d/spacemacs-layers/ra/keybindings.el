a2063d92a0e0b20ca0f42d8a8dd300ee702edf82
Ryan Artecona <ryanartecona@gmail.com>
2015-06-03 10:38:07 -0400

    make j/k behave better on wrapped lines

--- emacs.d/spacemacs-layers/ra/keybindings.el
+++ emacs.d/spacemacs-layers/ra/keybindings.el
@@ -1,6 +1,11 @@
+;; add some frame-related bindings
 (spacemacs/declare-prefix "wf" "frames")
 (evil-leader/set-key
   "wfn" 'make-frame-command
   "wfc" 'delete-frame
   "wfC" 'delete-other-frames
   "wfo" 'other-frame)
+
+;; make j/k work on visual lines instead of buffer lines (when wrapped)
+(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
