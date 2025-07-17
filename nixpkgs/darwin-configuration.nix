6a18163fe6093133aecfcacaf1c274ebf3a49ff9
Ryan Artecona <ryanartecona@Ryans-Mac-Studio.local>
2025-07-17 02:27:59 -0700

    add aerospace config woo

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -14,6 +14,80 @@
   system.primaryUser = "ryanartecona";
 
   services.lorri.enable = true;
+  
+  services.aerospace.enable = true;
+  services.aerospace.settings = {
+    accordion-padding = 50;
+    # https://nikitabobko.github.io/AeroSpace/guide#default-config
+    mode.main.binding = {
+      # un-float and tile this window, toggling horizontal/vertical
+      ctrl-slash = ["layout tiling" "layout tiles horizontal vertical"];
+      ctrl-cmd-slash = ["layout tiling" "layout tiles horizontal vertical"];
+      # un-float and accordion this window, toggling horizontal/vertical
+      ctrl-comma = ["layout tiling" "layout accordion horizontal vertical"];
+      ctrl-cmd-comma = ["layout tiling" "layout accordion horizontal vertical"];
+
+      ctrl-h = "focus left";
+      ctrl-j = "focus down";
+      ctrl-k = "focus up";
+      ctrl-l = "focus right";
+
+      ctrl-cmd-h = "move left";
+      ctrl-cmd-j = "move down";
+      ctrl-cmd-k = "move up";
+      ctrl-cmd-l = "move right";
+      
+      ctrl-minus = "resize smart -100";
+      ctrl-cmd-minus = "resize smart -100";
+      ctrl-equal = "resize smart +100";
+      ctrl-cmd-equal = "resize smart +100";
+
+      ctrl-1 = "workspace 1";
+      ctrl-2 = "workspace 2";
+      ctrl-3 = "workspace 3";
+      ctrl-4 = "workspace 4";
+      ctrl-5 = "workspace 5";
+      ctrl-6 = "workspace 6";
+      ctrl-7 = "workspace 7";
+      ctrl-8 = "workspace 8";
+      ctrl-9 = "workspace 9";
+
+      ctrl-cmd-1 = "move-node-to-workspace 1";
+      ctrl-cmd-2 = "move-node-to-workspace 2";
+      ctrl-cmd-3 = "move-node-to-workspace 3";
+      ctrl-cmd-4 = "move-node-to-workspace 4";
+      ctrl-cmd-5 = "move-node-to-workspace 5";
+      ctrl-cmd-6 = "move-node-to-workspace 6";
+      ctrl-cmd-7 = "move-node-to-workspace 7";
+      ctrl-cmd-8 = "move-node-to-workspace 8";
+      ctrl-cmd-9 = "move-node-to-workspace 9";
+
+      ctrl-tab = "workspace-back-and-forth";
+      ctrl-cmd-tab = "move-workspace-to-monitor --wrap-around next";
+
+      ctrl-cmd-space = "mode service";
+    };
+    mode.service.binding = {
+      esc = ["reload-config" "mode main"];
+      # reset layout
+      r = ["flatten-workspace-tree" "balance-sizes" "mode main"];
+      # Toggle between floating and tiling layout
+      f = ["layout floating tiling" "mode main"];
+      backspace = ["close-all-windows-but-current" "mode main"];
+
+      # sticky is not yet supported https://github.com/nikitabobko/AeroSpace/issues/2
+      #s = ["layout sticky tiling" "mode main"]
+
+      ctrl-cmd-h = ["join-with left" "mode main"];
+      ctrl-cmd-j = ["join-with down" "mode main"];
+      ctrl-cmd-k = ["join-with up" "mode main"];
+      ctrl-cmd-l = ["join-with right" "mode main"];
+
+      down = "volume down";
+      up = "volume up";
+      cmd-down = ["volume set 0" "mode main"];
+    };
+  };
 
   environment.systemPath = [
     "/opt/homebrew/bin"
