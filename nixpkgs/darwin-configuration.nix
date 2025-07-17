8f6b906aef8fe65964817075d20233ead7db5846
Ryan Artecona <ryanartecona@gmail.com>
2025-07-17 16:36:08 -0700

    aerospace tweaks

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 
+# Reference: https://nix-darwin.github.io/nix-darwin/manual/
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -18,6 +19,14 @@
   services.aerospace.enable = true;
   services.aerospace.settings = {
     accordion-padding = 50;
+    gaps = {
+      outer.top = 10;
+      outer.right = 10;
+      outer.bottom = 10;
+      outer.left = 10;
+      inner.horizontal = 10;
+      inner.vertical = 10;
+    };
     # https://nikitabobko.github.io/AeroSpace/guide#default-config
     mode.main.binding = {
       # un-float and tile this window, toggling horizontal/vertical
@@ -52,15 +61,15 @@
       ctrl-8 = "workspace 8";
       ctrl-9 = "workspace 9";
 
-      ctrl-cmd-1 = "move-node-to-workspace 1";
-      ctrl-cmd-2 = "move-node-to-workspace 2";
-      ctrl-cmd-3 = "move-node-to-workspace 3";
-      ctrl-cmd-4 = "move-node-to-workspace 4";
-      ctrl-cmd-5 = "move-node-to-workspace 5";
-      ctrl-cmd-6 = "move-node-to-workspace 6";
-      ctrl-cmd-7 = "move-node-to-workspace 7";
-      ctrl-cmd-8 = "move-node-to-workspace 8";
-      ctrl-cmd-9 = "move-node-to-workspace 9";
+      ctrl-cmd-1 = ["move-node-to-workspace 1" "workspace 1"];
+      ctrl-cmd-2 = ["move-node-to-workspace 2" "workspace 2"];
+      ctrl-cmd-3 = ["move-node-to-workspace 3" "workspace 3"];
+      ctrl-cmd-4 = ["move-node-to-workspace 4" "workspace 4"];
+      ctrl-cmd-5 = ["move-node-to-workspace 5" "workspace 5"];
+      ctrl-cmd-6 = ["move-node-to-workspace 6" "workspace 6"];
+      ctrl-cmd-7 = ["move-node-to-workspace 7" "workspace 7"];
+      ctrl-cmd-8 = ["move-node-to-workspace 8" "workspace 8"];
+      ctrl-cmd-9 = ["move-node-to-workspace 9" "workspace 9"];
 
       ctrl-tab = "workspace-back-and-forth";
       ctrl-cmd-tab = "move-workspace-to-monitor --wrap-around next";
@@ -83,12 +92,35 @@
       ctrl-cmd-k = ["join-with up" "mode main"];
       ctrl-cmd-l = ["join-with right" "mode main"];
 
+      ctrl-cmd-1 = "move-node-to-workspace 1";
+      ctrl-cmd-2 = "move-node-to-workspace 2";
+      ctrl-cmd-3 = "move-node-to-workspace 3";
+      ctrl-cmd-4 = "move-node-to-workspace 4";
+      ctrl-cmd-5 = "move-node-to-workspace 5";
+      ctrl-cmd-6 = "move-node-to-workspace 6";
+      ctrl-cmd-7 = "move-node-to-workspace 7";
+      ctrl-cmd-8 = "move-node-to-workspace 8";
+      ctrl-cmd-9 = "move-node-to-workspace 9";
+
       down = "volume down";
       up = "volume up";
       cmd-down = ["volume set 0" "mode main"];
     };
   };
 
+  services.jankyborders = {
+    enable = true;
+    # active_color = "0xffe1e3e4";
+    active_color = "glow(0xffd1aef1)";
+    inactive_color = "0xff494d64";
+    width = 4.0;
+    blur_radius = 10.0;
+    order = "above";
+    hidpi = true;
+  };
+
+  services.karabiner-elements.enable = true;
+  
   environment.systemPath = [
     "/opt/homebrew/bin"
     "/opt/homebrew/sbin"
@@ -100,6 +132,7 @@
 
   environment.shells = [
     pkgs.fish
+    "/Users/ryanartecona/.nix-profile/bin/fish"
   ];
 
   # Used for backwards compatibility, please read the changelog before changing.
@@ -107,5 +140,5 @@
   system.stateVersion = 6;
 
   # default nix installer uses gid 350 for nixbld group
-  ids.gids.nixbld = 350;
+  ids.gids.nixbld = 30000;
 }
